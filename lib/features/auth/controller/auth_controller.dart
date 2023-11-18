c4d52dcd92f5346842d6d508fd62cd529fa8f75a
PLWEP <langgeng86@gmail.com>
2023-11-18 13:44:44 +0700

    add firebase provider

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_reddit_clone/features/auth/repository/auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authControllerProvider = Provider((ref) => AuthController(authRepository: authRepository))
+
+class AuthController {
+  final AuthRepository _authRepository;
+
+  AuthController({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  void signInWithGoogle() {
+    _authRepository.signInWithGoogle();
+  }
+}
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,6 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    firestore: firestore,
+    auth: auth,
+    googleSignIn: googleSignIn,
+  ),
+);
 
 class AuthRepository {
   final FirebaseFirestore _firestore;
