fd45f6223ba7fb5109490ed9527f74432a0be099
PLWEP <langgeng86@gmail.com>
2023-11-18 14:08:11 +0700

    providerscope config

--- lib/common/sign_in_button.dart
+++ lib/common/sign_in_button.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/core/constant/constant.dart';
+import 'package:flutter_reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:flutter_reddit_clone/theme/pallete.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () => signInWithGoogle(ref),
         icon: Image.asset(
           Constant.googlePath,
           width: 35,
