e42d8155c164d8a29166d588d8dd27931c23f8a9
PLWEP <langgeng86@gmail.com>
2023-11-18 13:05:40 +0700

    multidex configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/screen/login_screen.dart';
 import 'package:flutter_reddit_clone/theme/pallete.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
