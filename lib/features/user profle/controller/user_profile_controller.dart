fb3a5db74576687ab7c4badaf07f2923f4bf4dcb
PLWEP <langgeng86@gmail.com>
2023-11-19 13:31:32 +0700

    add user profile controller

---
+++ lib/features/user profle/controller/user_profile_controller.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/core/provider/storage_repository_provider.dart';
+import 'package:flutter_reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:flutter_reddit_clone/features/user%20profle/repository/user_profile_repository.dart';
+import 'package:flutter_reddit_clone/models/user_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_reddit_clone/core/utils.dart';
+import 'package:routemaster/routemaster.dart';
+
+final userProfileControllerProvider =
+    StateNotifierProvider<UserProfileController, bool>(
+  (ref) {
+    final userProfileRepository = ref.watch(userProfileRepositoryProvider);
+    final storageRepository = ref.watch(storageRepositoryProvider);
+    return UserProfileController(
+      ref: ref,
+      userProfileRepository: userProfileRepository,
+      storageRepository: storageRepository,
+    );
+  },
+);
+
+class UserProfileController extends StateNotifier<bool> {
+  final UserProfileRepository _userProfileRepository;
+  final Ref _ref;
+  final StorageRepositorty _storageRepository;
+
+  UserProfileController(
+      {required UserProfileRepository userProfileRepository,
+      required Ref ref,
+      required StorageRepositorty storageRepository})
+      : _userProfileRepository = userProfileRepository,
+        _storageRepository = storageRepository,
+        _ref = ref,
+        super(false);
+
+  void editProfile({
+    required File? profileFile,
+    required File? bannerFile,
+    required BuildContext context,
+    required String name,
+  }) async {
+    UserModel user = _ref.read(userProvider)!;
+    state = true;
+    if (profileFile != null) {
+      final res = await _storageRepository.storeFile(
+        path: 'users/profile',
+        id: user.uid,
+        file: profileFile,
+      );
+
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => user = user.copyWith(profilePic: r),
+      );
+    }
+
+    if (bannerFile != null) {
+      final res = await _storageRepository.storeFile(
+        path: 'users/banner',
+        id: user.uid,
+        file: bannerFile,
+      );
+
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => user = user.copyWith(banner: r),
+      );
+    }
+
+    user = user.copyWith(name: name);
+    final res = await _userProfileRepository.editProfile(user);
+    state = false;
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        _ref.read(userProvider.notifier).update((state) => user);
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+}
