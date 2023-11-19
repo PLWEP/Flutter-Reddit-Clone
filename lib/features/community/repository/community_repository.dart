e67dd83495b20f0da88b8d44b5fa23722c4d9ae0
PLWEP <langgeng86@gmail.com>
2023-11-19 11:33:14 +0700

    add add mods feature pt2

--- lib/features/community/repository/community_repository.dart
+++ lib/features/community/repository/community_repository.dart
@@ -120,4 +120,18 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+
+  FutureVoid addMods(String communityName, List<String> uids) async {
+    try {
+      return right(
+        _communities.doc(communityName).update(
+          {'mods': uids},
+        ),
+      );
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
 }
--- lib/features/community/screen/add_mods_screen.dart
+++ lib/features/community/screen/add_mods_screen.dart
@@ -17,37 +17,70 @@ class AddModsScreen extends ConsumerStatefulWidget {
 }
 
 class _AddModsScreenState extends ConsumerState<AddModsScreen> {
+  Set<String> uids = {};
+
+  void addUid(String uid) {
+    setState(() {
+      uids.add(uid);
+    });
+  }
+
+  void removeUid(String uid) {
+    setState(() {
+      uids.remove(uid);
+    });
+  }
+
+  void saveMods() {
+    ref.read(communityControllerProvider.notifier).addMods(
+          widget.name,
+          uids.toList(),
+          context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.done),
+            onPressed: () => saveMods(),
+            icon: const Icon(Icons.done),
           ),
         ],
       ),
       body: ref.watch(getCommunityByNameProvider(widget.name)).when(
-            data: (data) => ListView.builder(
-              itemCount: data.members.length,
+            data: (community) => ListView.builder(
+              itemCount: community.members.length,
               itemBuilder: (context, index) {
-                final member = data.members[index];
+                final member = community.members[index];
 
                 return ref.watch(getUserDataProvider(member)).when(
-                      data: (data) => CheckboxListTile(
-                        value: true,
-                        onChanged: (value) {},
-                        title: Text(data.name),
-                      ),
+                      data: (data) {
+                        if (community.mods.contains(member)) {
+                          uids.add(member);
+                        }
+                        return CheckboxListTile(
+                          value: uids.contains(data.uid),
+                          onChanged: (value) {
+                            if (value!) {
+                              addUid(data.uid);
+                            } else {
+                              removeUid(data.uid);
+                            }
+                          },
+                          title: Text(data.name),
+                        );
+                      },
                       error: (error, stackTrace) =>
                           ErrorText(error: error.toString()),
-                      loading: () => Loader(),
+                      loading: () => const Loader(),
                     );
               },
             ),
             error: (error, stackTrace) => ErrorText(error: error.toString()),
-            loading: () => Loader(),
+            loading: () => const Loader(),
           ),
     );
   }
