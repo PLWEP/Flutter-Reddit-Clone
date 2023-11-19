282dbc91a51dfc62b5f34baa3c0e286d6268e915
PLWEP <langgeng86@gmail.com>
2023-11-19 10:38:58 +0700

    making profile drawer

---
+++ lib/features/home/drawers/profile_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:flutter_reddit_clone/theme/pallete.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfilDrawer extends ConsumerWidget {
+  const ProfilDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider);
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(user!.profilePic),
+              radius: 70,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              user.name,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Divider(),
+            ListTile(
+              title: const Text("My Profle"),
+              leading: const Icon(Icons.person),
+              onTap: () => null,
+            ),
+            ListTile(
+              title: const Text("Log Out"),
+              leading: Icon(
+                Icons.logout,
+                color: Pallete.redColor,
+              ),
+              onTap: () => null,
+            ),
+            Switch.adaptive(
+              value: true,
+              onChanged: (value) => null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
