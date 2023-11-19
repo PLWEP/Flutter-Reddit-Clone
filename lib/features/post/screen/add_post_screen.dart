7c16f32602fdc7e8b19e4dab54122490558da587
PLWEP <langgeng86@gmail.com>
2023-11-19 17:36:02 +0700

    making add post screen

--- lib/features/post/screen/add_post_screen.dart
+++ lib/features/post/screen/add_post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/theme/pallete.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddPostScreen extends ConsumerWidget {
@@ -6,6 +7,74 @@ class AddPostScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Column();
+    double cardHeightWidth = 120;
+    double iconSize = 60;
+    final currentTheme = ref.watch(themeNotifierProvider);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              color: currentTheme.colorScheme.background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 16,
+              child: Center(
+                child: Icon(
+                  Icons.image,
+                  size: iconSize,
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              color: currentTheme.colorScheme.background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 16,
+              child: Center(
+                child: Icon(
+                  Icons.font_download,
+                  size: iconSize,
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              color: currentTheme.colorScheme.background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 16,
+              child: Center(
+                child: Icon(
+                  Icons.link,
+                  size: iconSize,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
